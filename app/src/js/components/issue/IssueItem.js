79637951cda7ef1a8acac415e79e8ed4df3965e2
Gaston Niglia <gaston.niglia@xero.com>
2016-09-29 11:57:23 +1300

    changing thumbnail style

--- app/src/js/components/issue/IssueItem.js
+++ app/src/js/components/issue/IssueItem.js
@@ -6,7 +6,7 @@ import toastr from 'toastr';
 const IssueItem = ({issue, deleteIssueAction}) => {
   return (
     <div key={issue._id} class="col-sm-6 col-md-3">
-      <div class="thumbnail">
+      <div class="thumbnail" style={thumb}>
         <div class="caption">
           <h3><Link to={`/issues/${issue._id}`}>{issue.title}</Link></h3>
           <p>{issue.description} ({issue.comments.length})</p>
@@ -31,6 +31,12 @@ const IssueItem = ({issue, deleteIssueAction}) => {
       </div>
     </div>
   );
+};
+
+const thumb = {
+  width: '250px',
+  height: '300px',
+  overflow: 'auto'
 };
 
 export default IssueItem;
