2dd37beeaf5c28147da8e4349f34f3405fede5d8
Gaston Niglia <gaston.niglia@xero.com>
2016-10-20 15:11:37 +1300

    adding tests to searchBox

--- app/src/js/components/common/SearchBox.test.js
+++ app/src/js/components/common/SearchBox.test.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
 import SearchBox from './SearchBox';
 
-describe('Testing component <SearchBox />', () => {
-    it (` has Prop 'minLength' with '3' as text`, () => {
-      const wrapper = shallow(<SearchBox minLength={3} />);
-      expect(wrapper.props().minLength).to.eql(3);
+describe('Component <SearchBox />', () => {
+    it ('contains a <button> and not contain <input> control', () => {
+      const wrapper = shallow(<SearchBox buttonTest='Search' />);
+      expect(wrapper.find('button')).to.have.length(1);
+      expect(wrapper.find('input')).to.have.length(0);
+    });
+
+    it ('should have an initial filter state', () => {
+      const wrapper = mount(<SearchBox />);
+      expect(wrapper.state().filter).to.equal('');
+    });
+
+    it ('should have an initial showBox state', () => {
+      const wrapper = mount(<SearchBox />);
+      expect(wrapper.state().showBox).to.equal(false);
+    });
+
+    it('should update showBox state on clicking the search button', () => {
+      const wrapper = mount(<SearchBox/>);
+      wrapper.find('button').simulate('click');
+      expect(wrapper.state().showBox).to.equal(true);
+    });
+
+    it('should contains a <input> on clicking the search button', () => {
+      const wrapper = mount(<SearchBox/>);
+      wrapper.find('button').simulate('click');
+      expect(wrapper.find('input')).to.have.length(1);
     });
 });
