1a082b2c0bc67ae48db43bf14edeb698198af154
Gaston Niglia <gaston.niglia@xero.com>
2016-12-15 18:27:19 +1300

    Adding modal dialog

--- app/src/js/components/layout/Layout.js
+++ app/src/js/components/layout/Layout.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Header from './header/Header';
+import Modal from '../modal/Modal';
 
 const Layout = (props) => {
   return (
     <div>
+      <Modal />
+
       <Header />
       <section className='layout-main-section'>
         {props.children}
